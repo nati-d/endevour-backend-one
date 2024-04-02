e2c41a4c7f3acf134c04ac0a701eedeae3066d95
AbelMaireg <abelrighthere@gmail.com>
2024-04-02 20:35:36 +0300

    prisma migrations out of git control

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ app.use(express.json());
 
 app.use("/api/user", Router.user);
 
-// app.use("/api/admin", Router.adminRoutes);
+app.use("/api/admin", Router.adminRoutes);
 
 const port = 3000;
 app.listen(port, () => console.log("Server started at port", port));
