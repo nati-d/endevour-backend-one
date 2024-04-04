7de3f167454fef9de659562ce955af1934b168e5
AbelMaireg <abelrighthere@gmail.com>
2024-04-04 15:28:00 +0300

    get-job-post

--- src/controllers/job/createJobPost.ts
+++ src/controllers/job/createJobPost.ts
@@ -73,7 +73,6 @@ export default async (req: Request, res: Response) => {
         }
 
         try {
-            if (jobId != 0)
             await prisma.client.job_post.delete({
                 where: {
                     id: jobId
