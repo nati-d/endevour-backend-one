7803fb353da48e59b5fcf81fdfc4c8b4f0acb1e1
amanuel wonde <amanuelwt@gmail.com>
2024-04-06 09:54:49 +0300

    fixing cors error

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import Router from "./src/routers/index";
 const app = express();
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("this is home page"));
