14f51938a5be3a1987f402e6d91b1c104a6b605b
amanuel wonde <amanuelwt@gmail.com>
2024-04-24 18:13:18 +0300

    compiling ts to js for exclusive job

--- out/src/controllers/exclusive_job/sendEmailForRecommenders.js
+++ out/src/controllers/exclusive_job/sendEmailForRecommenders.js
@@ -25,12 +25,18 @@ const sendEmailForRecommenders = async (req, res, next) => {
                     in: mapedIds,
                 },
             },
+            select: {
+                email: true,
+            },
         });
-        const joinedEmails = recommendersEmail.join(",");
+        const joinedEmails = recommendersEmail
+            .map((recommender) => recommender.email)
+            .join(", ");
         req.emailData = {
             sendTo: joinedEmails,
             subject: "Recommend user for the above job",
-            html: updatedExclusiveJob.description,
+            html: updatedExclusiveJob.description +
+                `<a href='https://endevour.org/exclusive-job/recommend?job_id=${updatedExclusiveJob.id}'>Recommend here</a>`,
             queryOnFail: async () => await prismaClient_1.default.exclusive_job.update({
                 where: {
                     id: job_id,
