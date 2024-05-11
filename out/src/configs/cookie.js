6c4942f25c8a56fb4401fcd0d177f92c476b609c
AbelMaireg <abelrighthere@gmail.com>
2024-05-11 18:24:06 +0300

    pagination for getting job category added

--- out/src/configs/cookie.js
+++ out/src/configs/cookie.js
@@ -6,13 +6,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const redis_1 = require("redis");
 const connect_redis_1 = __importDefault(require("connect-redis"));
 let redisClient = (0, redis_1.createClient)();
-redisClient.connect().catch(console.error);
+redisClient.connect().catch();
 exports.default = {
     store: new connect_redis_1.default({
         client: redisClient,
         prefix: "endevour:"
     }),
-    secret: "your_secret_key_here",
+    secret: process.env.REDIS_SECRET,
     resave: false,
     saveUninitialized: false,
     cookie: {
--- src/controllers/job/catagory/getJobCategory.ts
+++ src/controllers/job/catagory/getJobCategory.ts
@@ -1,18 +1,50 @@
 import prisma from "../../../prisma/index";
+import { Prisma } from "@prisma/client";
 import { Request, Response } from "express";
+import _, { parseInt } from "lodash";
 import ApiResponse from "../../../types/response";
 
 export default async (req: Request, res: Response) => {
 
-    try {
-        const newCatagory = await prisma.client.job_category.findMany();
+  try {
 
-        res.send(new ApiResponse(true, "data fetched successfully", newCatagory));
+    let jobPosts: any;
+    let totalPages: number = 0;
+    let page = req.query.page ? (parseInt(req.query.page as string) - 1) * 10 : req.body.page ? (req.body.page - 1) * 10 : 0;
+    let currentPage = page ? page / 10 + 1 : 1;
 
-    } catch(error: any) {
+    jobPosts = await prisma.client.job_category.findMany({
+      take: 10,
+      skip: page,
+      orderBy: {
+        id: "desc",
+      },
+    });
 
-        return res.status(500).send(new ApiResponse(false, 'Unknown error while fetching data', error)); 
+    totalPages = await prisma.client.job_category.count();
 
+    totalPages = Math.ceil(totalPages / 10);
+
+    res.status(200).json(
+      new ApiResponse(true, "jop posts fetched successfully", {
+        job_category: jobPosts,
+        total_pages: totalPages,
+        current_page: currentPage,
+        next_page: currentPage >= totalPages ? null : currentPage + 1,
+      })
+    );
+  } catch (error) {
+    console.log(error);
+
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P2022")
+        return res
+          .status(400)
+          .json(new ApiResponse(false, "Not authorized to get get"));
     }
 
-}
+    return res
+      .status(500)
+      .json(new ApiResponse(false, "Error while fetching job post", error));
+  }
+};
