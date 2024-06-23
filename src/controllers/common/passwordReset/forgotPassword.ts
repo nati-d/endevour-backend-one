6c9bafb3686003b7c21c69c129526dc12412ffc6
Amanuel Wonde <amanuelwt@gmail.com>
2024-06-23 11:16:27 +0300

    fixing error while sending forgot password email

--- src/controllers/common/passwordReset/forgotPassword.ts
+++ src/controllers/common/passwordReset/forgotPassword.ts
@@ -1,9 +1,13 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import prisma from "../../../prisma/client/prismaClient";
 import ApiResponse from "../../../types/response";
 import sendEmail from "../../../services/notifications/sendEmail";
 
-const forgotPassword = async (req: Request, res: Response) => {
+const forgotPassword = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const { email, type } = req.body;
   let user;
 
@@ -39,12 +43,37 @@ const forgotPassword = async (req: Request, res: Response) => {
   }
 
   const confirmationCode = Math.floor(Math.random() * 10000) + 1;
+
   try {
-    await sendEmail(
-      email,
-      "Password Reset Code.",
-      `<p> Confirmation code</p>: <b>${confirmationCode}</b>`
-    );
+    const insertCode = await prisma.password_reset.create({
+      data: {
+        email,
+        confirmation_code: confirmationCode,
+      },
+    });
+
+    req.emailData = {
+      sendTo: email,
+      subject: "Password Reset Code.",
+      html: `<p> Confirmation code</p>: <b>${confirmationCode}</b>`,
+      async queryOnFail() {
+        await prisma.password_reset.delete({
+          where: {
+            id: insertCode.id,
+          },
+        });
+      },
+      resMessage: "Confirmation code has been send successfully.",
+      statusCode: 201,
+      otherData: { codeId: insertCode.id, userId: user.email },
+    };
+    // return res.status(201).json(
+    //   new ApiResponse(true, "Confirmation code has been send successfully.", {
+    //     codeId: insertCode.id,
+    //     userId: user.email,
+    //   })
+    // );
+    next();
   } catch (error) {
     console.log(error);
     return res
@@ -55,27 +84,6 @@ const forgotPassword = async (req: Request, res: Response) => {
           "Failed to send confirmation code please try later."
         )
       );
-  }
-
-  try {
-    const insertCode = await prisma.password_reset.create({
-      data: {
-        email,
-        confirmation_code: confirmationCode,
-      },
-    });
-
-    return res.status(201).json(
-      new ApiResponse(true, "Confirmation code has been send successfully.", {
-        codeId: insertCode.id,
-        userId: user.email,
-      })
-    );
-  } catch (error) {
-    console.log(error);
-    return res
-      .status(500)
-      .json(new ApiResponse(false, "Failed while managing confirmation code"));
   }
 };
 
