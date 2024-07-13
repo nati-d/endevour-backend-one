a9a01631d641512375c3893620fde17ccaad5f29
AbelMaireg <abelrighthere@gmail.com>
2024-07-13 10:32:12 +0300

    -a

--- src/controllers/user/googleLogin.ts
+++ src/controllers/user/googleLogin.ts
@@ -24,7 +24,7 @@ export default async (req: Request, res: Response) => {
     } catch(error) {
         console.error(error);
 
-        res.status(500).json(new ApiResponse(false, 'error while processing request'));
+        return res.status(500).json(new ApiResponse(false, 'error while processing request'));
     }
 
     try {
