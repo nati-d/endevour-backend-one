d499b537728665d0ffe61d71f19d6bcc13c254bb
AbelMaireg <abelrighthere@gmail.com>
2024-07-13 22:14:36 +0300

    -a

--- out/src/controllers/home.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = async (req, res) => {
-    console.log(req);
-    res.send('hello');
-    // res.redirect('https://www.endevour.org');
-};
